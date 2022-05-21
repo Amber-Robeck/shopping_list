5500ab8573cbf48af495344d3c1611547a8fb352
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-21 13:28:42 -0500

    Did not have ItemList file saved, saved and now added.

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import DummyData from '../DummyData';
+// import DummyData from '../DummyData';
 import { FaTrashAlt } from 'react-icons/fa';
 import './itemList.css'
 
